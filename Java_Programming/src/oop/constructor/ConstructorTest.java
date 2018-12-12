328621f3f878816ba1a1c13066b97d21b979377c
ashima bhola <denishbhola970@gmail.com>
2018-12-12 13:11:26 +0530

    @ Second Commit

---
+++ Java_Programming/src/oop/constructor/ConstructorTest.java
@@ -0,0 +1,17 @@
+package oop.constructor;
+
+public class ConstructorTest {
+
+	public ConstructorTest() {
+		// TODO Auto-generated constructor stub
+		System.out.println("HJiiiii Constr");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		new ConstructorTest();
+		
+
+	}
+
+}
