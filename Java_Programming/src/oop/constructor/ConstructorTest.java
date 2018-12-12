9645c6d1aafacf9164901ac26ab1731039ea5f56
ashima bhola <denishbhola970@gmail.com>
2018-12-12 13:15:53 +0530

    changed the object

--- Java_Programming/src/oop/constructor/ConstructorTest.java
+++ Java_Programming/src/oop/constructor/ConstructorTest.java
@@ -9,7 +9,7 @@ public class ConstructorTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		new ConstructorTest();
+		ConstructorTest v = new ConstructorTest();
 		
 
 	}
